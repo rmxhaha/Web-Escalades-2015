64cbb69c3c58c696de93e752e6a7adc4859623f3
Jonathan Hosea <jonathan.hosea@me.com>
2015-08-08 11:07:36 +0700

    Added live score

---
+++ Live Score/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+
+<!-- Metas -->
+<meta charset="UTF-8">
+
+<!-- Title -->
+<title>Escalades Vidias - Live Score</title>
+
+<!-- Stylesheets -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!-- Scripts -->
+<script>
+
+
+</script>
+</head>
+<body>
+	<div class="wrapper">
+    	<div class="centerBox">
+        	<div class="boxHeader">
+            The Title Of the game goes here
+            </div>
+    		<div class="boxOne">
+            	<div class="header">
+            	School Name Goes Here
+            	</div>
+    		score goes here
+            </div>
+            <div class="boxTwo">
+            	<div class="header">
+            	School Name Goes Here
+            	</div>
+    		score goes here
+            </div>
+    	</div>
+    </div>
+</body>
+</html>
